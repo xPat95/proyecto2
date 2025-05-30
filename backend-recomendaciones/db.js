800b5a3003939532f686d67ffec404611222ed55
Chon <lee241203@uvg.edu.gt>
2025-05-29 22:39:00 -0600

    documentacion db

--- backend-recomendaciones/db.js
+++ backend-recomendaciones/db.js
@@ -1,6 +1,8 @@
-const neo4j = require('neo4j-driver');
-require('dotenv').config();
+//Importar dependencias
+const neo4j = require('neo4j-driver'); //conexion para ejecutar consultas desde Node.js con la libreria oficial de Neo4j.
+require('dotenv').config(); //Carga variables de entorno desde el archivo .env
 
+//Configuracion Neo4j
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
