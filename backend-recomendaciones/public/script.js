f28afe036e9f8d2af45651351c602aa830a3dc33
Chon <lee241203@uvg.edu.gt>
2025-05-29 22:50:20 -0600

    documentacion script

--- backend-recomendaciones/public/script.js
+++ backend-recomendaciones/public/script.js
@@ -1,6 +1,10 @@
+//Registrar usuario y preferencia.
+//Agrega un listener al formulario ID form-registro para interceptar el evento submit.
 document.getElementById('form-registro').addEventListener('submit', async (e) => {
   e.preventDefault();
 
+//Lectura y validar campos
+//Obtiene los valores y luego los valida.
   const nombre = document.getElementById('nombre').value.trim();
   const pelicula = document.getElementById('pelicula').value.trim();
   const genero = document.getElementById('genero').value.trim();
@@ -10,6 +14,8 @@ document.getElementById('form-registro').addEventListener('submit', async (e) =>
     return;
   }
 
+  //Envio de datos a API.
+  //Envia la solicitud POST a la ruta /registro-completo del servidor con los datos en formato JSON.
   const res = await fetch('/registro-completo', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -19,11 +25,15 @@ document.getElementById('form-registro').addEventListener('submit', async (e) =>
   alert(res.ok ? 'Registro completo exitoso' : 'Error al registrar');
 });
 
+//Obtener recomendacion de peliculas
+//Llama desde  un botno o evento para obtener recomendaciones para el usuario. Toma el nombre del usuario desde un campo con ID usuarioRecomendado.
+//Luego realiza una solicitud GET a /recomendar/:nombre y lo convierte a JSON.
 async function obtenerRecomendaciones() {
   const nombre = document.getElementById('usuarioRecomendado').value.trim();
   const res = await fetch(`/recomendar/${nombre}`);
   const datos = await res.json();
 
+  //Muestra el resultado en la pagina.
   const lista = document.getElementById('lista-recomendaciones');
   lista.innerHTML = '';
 
